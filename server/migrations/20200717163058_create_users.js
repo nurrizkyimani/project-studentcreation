21b7267398aa18f05864af456a845216f322e8fc
bocahrokok <indieunistudy@gmail.com>
2020-07-17 22:27:36 +0700

    add knex database schema

--- server/migrations/20200717163058_create_users.js
+++ server/migrations/20200717163058_create_users.js
@@ -1,12 +1,21 @@
 
 exports.up = function(knex, Promise) {
-    return Promise.all([
+  return Promise.all([
+    knex.schema.createTable('users', table => {
+      table.increment('id').primary()
+      table.string('name').notNullable()
+      table.string('faculty').notNullable()
+      table.string('department')
+      table.string('year')
+      table.string('linkedin')
+      table.string('line')
+      })
       
   ])
 };
 
-exports.down = function(knex) {
+exports.down = function(knex, Promise) {
   return Promise.all([
-      
+    knex.schema.dropTable('users')
   ])
 };
--- server/migrations/20200717163109_create_projects.js
+++ server/migrations/20200717163109_create_projects.js
@@ -1,8 +1,18 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex, Promise) {
+    Promise.all([
+        knex.schema.createTable('projects', table => {
+            table.uuid('id').primary()
+            table.string('title').notNullable()
+            table.text('description')
+            table.string('topic')
+            table.integer('member')
+        })
+    ])
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+    Promise.all([
+    knex.schema.dropTable('projects')
+  ])
 };
